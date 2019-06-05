63947029876b805e6ff9b793a929f6ccfa2d5384
Philipp <philipp.rolli@t-online.de>
2019-06-05 22:04:42 +0200

    t

--- Main.java
+++ Main.java
@@ -1,15 +1,15 @@
 import processing.core.*;
 
-public class test extends PApplet{
+public class Main extends PApplet{
     public static void main(String args[]){
-        PApplet.main(new String[] {"--present", "test"});
+        PApplet.main(new String[] {"--present", "Main"});
     }
-
+        
     public void settings(){
-
+        size(200,200);
     }
-
+    
     public void draw(){
-
+        
     }
 }
