5b388f6786ce0d6f6f4c295dc2fccf9d65984490
Philipp <philipp.rolli@t-online.de>
2019-06-05 21:13:56 +0200

    Initial sharing of project

---
+++ test.java
@@ -0,0 +1,15 @@
+import processing.core.*;
+
+public class test extends PApplet{
+    public static void main(String args[]){
+        PApplet.main(new String[] {"--present", "test"});
+    }
+
+    public void settings(){
+
+    }
+
+    public void draw(){
+
+    }
+}
